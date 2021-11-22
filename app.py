e2374d90bbe9bfdef41ea4552e19d66ee6c2b2f5
Hyelim <hlchoi1223@gmail.com>
2021-11-22 13:57:03 +0900

    [Edit] 프론트 엔드 + 백엔드 연결

--- app.py
+++ app.py
@@ -3,21 +3,33 @@ from flask import Flask, redirect, url_for, request, render_template, session
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route("/")
 def index():
-    blogger = {'name': 'jvvp', 'eloc': 'songpa'}
+    blogger = {"name": "jvvp", "eloc": "songpa"}
 
-    return render_template('index.html', suggestion=blogger)
+    return render_template("index.html", suggestion=blogger)
 
-@app.route('/layout')
+
+@app.route("/layout")
 def layout():
-    return render_template('layout.html')
+    return render_template("layout.html")
 
-@app.route('/result', methods=['POST'])
+
+@app.route("/home", methods=["GET", "POST"])
+def home():
+    if request.method == "POST":
+        data = request.form["description"]
+        return render_template("home.html", description=data)
+
+    return render_template("home.html")
+
+
+@app.route("/result", methods=["POST"])
 def result():
-    data = request.form['word']
+    data = request.form["word"]
     print(data)
     return data
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000, debug=True)
