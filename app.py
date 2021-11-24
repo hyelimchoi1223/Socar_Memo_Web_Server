b8d568d3380f3e45b54691f14d0048ae33a299db
Hyelim <hlchoi1223@gmail.com>
2021-11-24 19:15:25 +0900

    [Edit] 카테고리 기능 추가

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, redirect, url_for, request, render_template, session
+import requests
+import json
+from operator import itemgetter
 
 app = Flask(__name__)
+
+rest_url = "http://34.132.153.232:5000/"
 
 
 @app.route("/")
@@ -17,11 +22,19 @@ def layout():
 
 @app.route("/home", methods=["GET", "POST"])
 def home():
+    data = {}
     if request.method == "POST":
-        data = request.form["description"]
+        description = request.form["description"]
+        # description = "미션 교체 카맨모터스 법인카드 결제건"
+        res = requests.get(f"{rest_url}classify?desc={description}")
+        data = res.content.decode("unicode-escape")
+        data = json.loads(data)
+        data = sorted(data.items(), key=(lambda x: x[1]), reverse=True)
+        data = dict(data[:5])
+        data = {k: round(v, 2) * 100 for k, v in data.items()}
         return render_template("home.html", description=data)
 
-    return render_template("home.html")
+    return render_template("home.html", description=data)
 
 
 @app.route("/result", methods=["POST"])
